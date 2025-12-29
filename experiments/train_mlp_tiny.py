ba199375d21b179c5c8dd266c4f23f212ae0ab43
banatehrani <banatehrnai@gmail.com>
2025-12-29 18:21:04 -0500

    feat: add tiny MLP training script

---
+++ experiments/train_mlp_tiny.py
@@ -0,0 +1,37 @@
+from micrograd_notes.engine import Value
+from micrograd_notes.nn import MLP
+
+# Tiny dataset (Karpathy style)
+xs = [
+    [2.0, 3.0, -1.0],
+    [3.0, -1.0, 0.5],
+    [0.5, 1.0, 1.0],
+    [1.0, 1.0, -1.0],
+]
+ys = [1.0, -1.0, -1.0, 1.0]
+
+mlp = MLP(3, [4, 4, 1])
+lr = 0.05
+
+for k in range(50):
+    # forward
+    ypred = [mlp([Value(xi) for xi in x]) for x in xs]
+
+    # loss: sum of squared error
+    loss = sum((yout - ygt) ** 2 for yout, ygt in zip(ypred, ys))
+
+    # backward
+    mlp.zero_grad()
+    loss.backward()
+
+    # update
+    for p in mlp.parameters():
+        p.data += -lr * p.grad
+
+    if k % 5 == 0:
+        print(k, loss.data)
+
+print("predictions:")
+for x in xs:
+    y = mlp([Value(xi) for xi in x])
+    print(x, y.data)
